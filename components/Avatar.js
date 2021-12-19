f59145ee7fc74723f49e82e097a607bfd1dfe9eb
Al Mahamud Murad <muradhasan.7845@gmail.com>
2021-12-19 18:44:28 +0600

    addTooltip

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,16 +1,22 @@
 import Image from "next/image";
 import { useMoralis } from "react-moralis";
+import ReactTooltip from "react-tooltip";
 
 export const Avatar = ({ username, logoutOnPress }) => {
   const { user, logout } = useMoralis();
   return (
-    <Image
-      className="rounded-full bg-black cursor-pointer"
-      src={`https://avatars.dicebear.com/api/personas/${
-        username || user.get("username")
-      }.svg`}
-      layout="fill"
-      onClick={() => logoutOnPress && logout()}
-    />
+    <>
+      <Image
+        className="rounded-full bg-black cursor-pointer"
+        src={`https://avatars.dicebear.com/api/personas/${
+          username || user.get("username")
+        }.svg`}
+        layout="fill"
+        data-tip="Click to Logout"
+        data-for="test"
+        onClick={() => logoutOnPress && logout()}
+      />
+      <ReactTooltip id="test" place="right" type="dark" effect="solid" />
+    </>
   );
 };
