771fa36e1ded47553bdebafd72d0aac388721dba
Al Mahamud Murad <muradhasan.7845@gmail.com>
2021-12-20 20:55:44 +0600

    update

--- components/Message.js
+++ components/Message.js
@@ -5,15 +5,19 @@ import { Avatar } from "./Avatar";
 function Message({ message }) {
   const { user } = useMoralis();
 
-  const isUserMessage = message.get("ethAddress") === user?.get("ethAddress");
+  const isUserMessage =
+    message.get("ethAddress || username") ===
+    user?.get("ethAddress || username");
 
   return (
-    <div className={`flex items-end space-x-2 relative ${isUserMessage && "justify-end"}` }>
-        <div className={`relative h-8 w-8 ${
-            isUserMessage && "order-last ml-2"
-        }`}>
-            <Avatar username={message.get("username")}/>
-        </div>
+    <div
+      className={`flex items-end space-x-2 relative ${
+        isUserMessage && "justify-end"
+      }`}
+    >
+      <div className={`relative h-8 w-8 ${isUserMessage && "order-last ml-2"}`}>
+        <Avatar username={message.get("username")} />
+      </div>
       <div
         className={`flex space-x-4 p-3 rounded-lg ${
           isUserMessage
@@ -24,13 +28,18 @@ function Message({ message }) {
         <p>{message.get("message")}</p>
       </div>
       {/* time stamp */}
-      <TimeAgo className={`text-[10px] italic text-gray-400 ${
-          isUserMessage && 'order-first pr-1'
-      }`} datetime={message.createdAt}/>
-      <p className={`absolute -bottom-5 text-xs ${
-        isUserMessage ? "text-pink-300" : "text-blue-400"
-      }`}>
-          {message.get("username")}
+      <TimeAgo
+        className={`text-[10px] italic text-gray-400 ${
+          isUserMessage && "order-first pr-1"
+        }`}
+        datetime={message.createdAt}
+      />
+      <p
+        className={`absolute -bottom-5 text-xs ${
+          isUserMessage ? "text-pink-300" : "text-blue-400"
+        }`}
+      >
+        {message.get("username")}
       </p>
     </div>
   );
